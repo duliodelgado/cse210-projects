eb43ad092e737af69db8fbb569efabb35bca91a9
duliodelgado <dulio.delgado@gmail.com>
2025-02-01 10:47:41 -0500

    go

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Technician";
+        job1._company = "IBM";
+        job1._startYear = 2000;
+        job1._endYear = 2003;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Project Manager Assistant";
+        job2._company = "Nokia";
+        job2._startYear = 2003;
+        job2._endYear = 2005;
+
+        Resume myResume = new Resume();
+        myResume._name = "Dulio Delgado";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
